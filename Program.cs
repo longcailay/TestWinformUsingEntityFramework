500da0a0471325155b2c265816bbab1bf0c1a27b
longcailay <longcailay@gmail.com>
2020-04-10 15:16:40 +0700

    Cap nhat bang ghi

--- Program.cs
+++ Program.cs
@@ -22,6 +22,8 @@ namespace P01_HelloCodeFirst
 
         public DbSet<Person> People { get; set; }
     }
+    
+
     static class Program
     {
         /// <summary>
@@ -34,18 +36,37 @@ namespace P01_HelloCodeFirst
             {
                 context.Database.CreateIfNotExists();
 
-                var person = new Person
-                {
-                    FirstName = "Donald",
-                    LastName = "Trump"
-                };
-                context.People.Add(person);
-                context.SaveChanges();
+                Console.WriteLine("Before:");
+                Retrieve(context);
+                Update(context);
+                Console.WriteLine("After:");
+                Retrieve(context);
+
             }
+
+
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+        private static void Update(Context context)
+        {
+            var person = context.People.FirstOrDefault();
+            if (person != null)
+            {
+                person.FirstName = "Barrack";
+                person.LastName = "Obama";
+                context.SaveChanges();
+            }
+        }
+        private static void Retrieve(Context context)
+        {
+            var people = context.People;
+            foreach (var person in people)
+            {
+                Console.WriteLine($"[{person.PersonId}] {person.FirstName} {person.LastName}" + "    dddd");
+            }
         }
     }
     
