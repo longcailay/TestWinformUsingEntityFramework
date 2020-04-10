9bc3d7879fa92dbfc2d014703be6ac57b42a745f
longcailay <longcailay@gmail.com>
2020-04-10 15:09:25 +0700

    Xong phan truy van du lieu

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P01_HelloCodeFirst
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class Context : DbContext
+    {
+        public Context() : base("name=S03_connectionString")
+        {
+
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var context = new Context())
+            {
+                context.Database.CreateIfNotExists();
+
+                var person = new Person
+                {
+                    FirstName = "Donald",
+                    LastName = "Trump"
+                };
+                context.People.Add(person);
+                context.SaveChanges();
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+    
+   
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TestWinformUsingEntityFramework
 Ứng dụng đơn giản Winform sử dụng Entity Framework
+
+Phải bật máy ảo window lên để chạy Sql server
